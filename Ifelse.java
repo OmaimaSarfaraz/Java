88691fd6f1463b63635c38c73c47acaa1a360449
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-08 20:12:28 +0500

    If-Else Conditional Statement

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+package controlstatements;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		if(x>5)
+		{
+			System.out.println("Greater");
+			}
+		else{
+			System.out.println("Smaller");
+			}
+		}
+	}
