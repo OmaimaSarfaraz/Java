d43d6bcea425fe1338eef3fc5372b5498ce8ee41
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-12 11:26:49 +0500

    Lab Tasks 03

---
+++ MainInherit.java
@@ -0,0 +1,10 @@
+package inhertance1;
+
+public class MainInherit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Student a=new Student();
+        a.introduce();
+	}
+}
