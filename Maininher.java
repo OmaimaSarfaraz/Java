caac2b7cb2eca96e458cd7e1dbb45323d414c202
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-13 20:13:02 +0500

    Multi Level Inheritance

---
+++ Maininher.java
@@ -0,0 +1,12 @@
+package multilevelinheritance;
+
+public class Maininher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         GrandChild greet=new GrandChild();
+         greet.greet();
+         greet.greeting();
+         greet.greets();
+	}
+}
