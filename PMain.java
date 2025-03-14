7ad90c1ab3c9682b47d732d74c29c5743bf297e1
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-14 17:08:57 +0500

    Polymorphism Class Add

--- PMain.java
+++ PMain.java
@@ -4,7 +4,7 @@ public class PMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Calc c=new Calc();
+		Calcul c=new Calcul();
         c.add(52, 23);
         c.add(5, 10, 250);
 	}
