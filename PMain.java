cd62b4b39274ee119bf1029f56fcd480fd346085
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-14 17:06:56 +0500

    Polymorphism Main Class

---
+++ PMain.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class PMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calc c=new Calc();
+        c.add(52, 23);
+        c.add(5, 10, 250);
+	}
+}
