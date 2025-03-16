1e984159b6862210f18b8312d82e08b86f826b9d
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-16 22:20:04 +0500

    Double Data Type Input

---
+++ DoubleInput.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.Scanner;
+
+public class DoubleInput {
+
+	public static void main(String[] args) {
+		
+				// TODO Auto-generated method stub
+				Scanner input=new Scanner(System.in);
+		        double s;
+		        System.out.println("Enter Number with decimal: ");
+		        s=input.nextDouble();
+		        System.out.println("Number is: "+s);
+	}
+
+}
