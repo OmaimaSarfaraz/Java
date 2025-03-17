ac5e2aa33e3c1897478900def6d4405fa7fe0455
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-17 20:33:46 +0500

    Void Function Main Class

---
+++ Function.java
@@ -0,0 +1,10 @@
+package functions;
+
+public class Function {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VoidFunction.greet();
+		System.out.print(ReturnFunction.getMessage());
+	}
+}
