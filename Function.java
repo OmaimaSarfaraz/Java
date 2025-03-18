92348837b17516cdfc3e07730c22f0de4b469071
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-18 22:54:15 +0500

    Method with Parameters

--- Function.java
+++ Function.java
@@ -5,6 +5,8 @@ public class Function {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VoidFunction.greet();
-		System.out.print(ReturnFunction.getMessage());
+		System.out.println(ReturnFunction.getMessage());
+		ParameterFunction.sum(50,10);
+		System.out.println(ParameterReturnFunction.multiply(32,5));
 	}
 }
