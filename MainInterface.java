f26cb0263d8f21eafd8f98a2499f02f9a8c3e011
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-20 23:33:15 +0500

    Lab 04 Task # 03

---
+++ MainInterface.java
@@ -0,0 +1,12 @@
+package Lab04;
+
+public class MainInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Dog a=new Dog();
+        a.makesound();
+        Cat b=new Cat();
+        b.makesound();
+	}
+}
