62efb7250e9adcbe1ee45445f0e5bbdddc407dcd
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-23 12:15:09 +0500

    Main Class And Objects

---
+++ ObjectMain.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class ObjectMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cars c=new Cars();
+        c.Car("Tesla");
+        c.show();
+        System.out.println("Year:"+c.year);
+	}
+}
