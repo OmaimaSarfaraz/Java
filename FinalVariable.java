e754b9bddb9d5697c533062728f4829359949dc1
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-26 19:53:53 +0500

    Lab 05 Task # 01

---
+++ FinalVariable.java
@@ -0,0 +1,10 @@
+package Lab05;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double pi=3.14;
+		pi=8.8; //Variable pi can not be reassigned because of the final keyword which make it constant
+	}
+}
