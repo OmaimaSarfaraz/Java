427847a558fe77273c0b51dddfd25db661596ca1
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-28 21:51:01 +0500

    Protected Access Modifier

---
+++ CarClassProtected.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class CarClassProtected {
+	protected String model="Ferrari";
+	protected void display(){
+		System.out.println("Model:"+model);
+		}
+}
---
+++ ProtectedAccessModifier.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class ProtectedAccessModifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        CarClassProtected car=new CarClassProtected();
+        car.display();
+	}
+}
