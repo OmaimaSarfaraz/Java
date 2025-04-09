dbe79b98521c47733201f785f25e29f7bca63bed
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-04-09 20:17:01 +0500

    Nested If Statement

---
+++ NestedIf.java
@@ -0,0 +1,12 @@
+package controlstatements;
+
+public class NestedIf{
+	public static void main(String[]args){
+		int x=10; 
+		if(x>0){
+			if(x%2==0){
+				System.out.println("Positive Even");
+				}
+			}
+		}
+}
