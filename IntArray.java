21fc074ae0fa58fa6c8b3462fb3fb9ac6b56af96
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-04-15 18:34:15 +0500

    Array Integer

---
+++ IntArray.java
@@ -0,0 +1,12 @@
+package dataTypes;
+
+public class IntArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numbers[] = {10,20,30,40,50};
+		for (int i=0; i<numbers.length; i++) {
+            System.out.println("Element at index "+i+": "+numbers[i]);
+        }
+	}
+}
