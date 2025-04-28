575351d0cd8aedb457fc4917a7c634c9fee3ca1e
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-04-28 21:40:32 +0500

    Main Class

---
+++ ShapeMain.java
@@ -0,0 +1,11 @@
+package inherit;
+
+public class ShapeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c=new Circle();
+		c.display();
+		c.area();
+	}
+}
