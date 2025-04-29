9b0d7a20a8b8dbd686779a1e09cb8a4dfc73a027
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-04-29 18:45:15 +0500

    Overloading

---
+++ MessageMain.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class MessageMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MessageOverload.message();
+		MessageOverload.message("Welcome!");
+		MessageOverload.message("Hello",3);
+	}
+}
