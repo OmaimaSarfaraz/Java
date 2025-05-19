0bbeb2556ded2b210aa43fbb642b8c4769efdf45
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-05-19 21:23:14 +0500

    Abstaction Example

---
+++ ClassAbstract.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class ClassAbstract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t;
+		t=new Adds();
+		System.out.println(t.calc());
+		t=new Subs();
+		System.out.println(t.calc());
+	}
+}
