9ea71e8804533bbdb8e3d05dab5ba0e826a20e49
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-05-26 13:41:30 +0500

    Inheit

---
+++ DancingRobot.java
@@ -0,0 +1,5 @@
+package inhertance1;
+
+public class DancingRobot extends Robot{
+	void dance(){System.out.println("Robot is dancing! 💃🤖");}
+}
---
+++ RobotTest.java
@@ -0,0 +1,12 @@
+package inhertance1;
+
+public class RobotTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DancingRobot d=new DancingRobot();
+		d.powerOn();
+		d.move();
+		d.dance();
+	}
+}
