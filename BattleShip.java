42e1aafc9ec07d9c398ca9da8e770c227ae4d06d
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-05-27 20:10:06 +0500

    Practice

---
+++ BattleShip.java
@@ -0,0 +1,10 @@
+package inherit;
+
+public class BattleShip  extends SpaceShip{
+	void activateWeapons(){
+		System.out.println("🔫 Weapons activated!");
+		}
+	void attack(){
+		System.out.println("💥 Attacking enemy ship!");
+		}
+}
