a981338f176efb59700b38a11619734ab1a68f46
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-05-27 20:10:36 +0500

    Practice

---
+++ GalaxyMission.java
@@ -0,0 +1,13 @@
+package inherit;
+
+public class GalaxyMission {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BattleShip ship=new BattleShip();
+		ship.launch();
+		ship.navigate();
+		ship.activateWeapons();
+		ship.attack();
+	}
+}
