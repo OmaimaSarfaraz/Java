ae69bea7c3eccfe7430f6de0ad2641003f4fe6cd
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-06-21 09:36:15 +0500

    Main

---
+++ ConstructorMain.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class ConstructorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Constructor p = new Constructor("Tasha", 22);
+        p.display();
+	}
+}
