6b5b6c12b6fd3c8fec8c4e2e2ea7fdd542b255b0
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-06-23 07:24:27 +0500

    Main Count

---
+++ MainCounter.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class MainCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Counter();
+        new Counter();
+        new Counter();
+        Counter.showCount();
+	}
+}
