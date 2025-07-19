447bded7d7a1569d2a9633ae217ff07b426446f0
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-07-19 10:53:20 +0500

    Practice

---
+++ Clock.java
@@ -0,0 +1,14 @@
+package Practice;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Clock {
+    public static void main(String[] args) throws InterruptedException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        while (true) {
+            System.out.print("\rCurrent Time: " + LocalTime.now().format(formatter));
+            Thread.sleep(1000);
+        }
+    }
+}
