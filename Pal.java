c217cc4a2d3ce235d5039cfb94e0185d0e65270f
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-11-04 21:13:12 +0500

    Palindrome

---
+++ Pal.java
@@ -0,0 +1,13 @@
+public class Pal{
+    public static void main(String[]a){
+        int n=121,t=n,r=0;
+        while(n>0){
+            r=r*10+n%10;
+            n/=10;
+        }
+        if(t==r)
+            System.out.println("Palindrome");
+        else 
+            System.out.println("Not");
+    }
+}
