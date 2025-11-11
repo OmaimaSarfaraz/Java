abd376ef02ce1e99fe1d9c5c043f718209d339fe
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-11-11 18:29:05 +0500

    Num

---
+++ Large.java
@@ -0,0 +1,11 @@
+public class Large {
+    public static void main(String[]a){
+        int x=5,y=8,z=3;
+        if(x>y&&x>z)
+            System.out.println(x);
+        else if(y>z)
+            System.out.println(y);
+        else 
+            System.out.println(z);
+    }
+}
