8b2d187d92875e122a221069a86ada434104fc94
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-11-30 19:05:44 +0500

    Search

---
+++ LinSearch.java
@@ -0,0 +1,8 @@
+public class LinSearch{
+    public static void main(String[]a){
+        int arr[]={3,5,7,2},key=7;
+        for(int i=0;i<arr.length;i++)
+            if(arr[i]==key)
+                System.out.println("Found");
+    }
+}
