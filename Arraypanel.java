9aa02c45f734d1ab66a776c1898a24425f8b5ec3
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-12-19 22:42:54 +0500

    Project

---
+++ Arraypanel.java
@@ -0,0 +1,45 @@
+package Dsaconceptvisualizer;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Arraypanel extends JPanel {
+
+    private int[] arr;
+
+    public void setArray(int[] arr) {
+        this.arr = arr;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (arr == null) return;
+
+        int x = 20;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // Draw box
+            g.setColor(Color.BLACK);
+            g.drawRect(x, 50, 60, 60);
+
+            // Display value
+            if (arr[i] != -1) {
+                g.setFont(new Font("Arial", Font.BOLD, 18));
+                g.drawString(String.valueOf(arr[i]), x + 22, 85);
+            }
+
+            // Index below
+            g.setFont(new Font("Arial", Font.PLAIN, 12));
+            g.drawString("[" + i + "]", x + 20, 130);
+
+            x += 80;
+        }
+    }
+}
+
+
