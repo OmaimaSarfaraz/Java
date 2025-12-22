b236b65dc601fc46962d1eec94aac9c85fc06513
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-12-22 18:28:56 +0500

    Condition

---
+++ PassFail.java
@@ -0,0 +1,9 @@
+public class PassFail{
+    public static void main(String[] args){
+        int marks=60;
+        if(marks>=50) 
+            System.out.println("Pass");
+        else 
+            System.out.println("Fail");
+    }
+}
